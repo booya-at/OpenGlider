06403635c4e7c5f8b2d1576a8a6677a47cdeada2
looooo <sppedflyer@gmail.com>
2013-07-28 18:49:06 +0200

    lo

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import Profile
 import Vector
-#from .Ribs import Rib
-##import .Profile
-neu=[[1,0],[0.5,0.1],[0,0],[0.5,-0.1],[1,0]]
-
-import Profile
+import Graphics as G
+gobj = G.Line([[0,0,1],[0,0,2]])
+print(gobj.coordinates)
+gobj._SetColour('green')
+print(gobj.colour)
