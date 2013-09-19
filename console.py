5af522e463856d717d2d6589fb9f1b697d5dc917
looooo <sppedflyer@gmail.com>
2013-09-19 11:19:52 +0200

    lo

---
+++ console.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys
+import os
+from Graphics import *
+from Profile import *
+import numpy as np
+
+#clear the terminal
+os.system('clear')
+
+#custom prompt
+sys.ps1 = ">"
+sys.ps2 = '>>'
+os.environ['PYTHONINSPECT'] = 'True'
+print(
+"                     xxxxxxxxxxxxxxx                                  \n"
+"                xxxxxxxxxxxxxxxxxxxxxxx                               \n"
+"            xxxxxxxxxxx       x        x                              \n"
+"        xxxxxxxxxxx  x       xx       xx                              \n"
+"      xxxxxxxxx     xx        x       x                               \n"
+"    xxxxxxxx          xx      x       x    xxxxxxxxx       xxxxxxxxxxx\n"
+"  xxxxxxxx  x           x      x      x   xxx     xxxx    xxx         \n"
+" xxxxxxx    xx           x      x     x  xx         xx   xxx          \n"
+"xxxxxx                    xx    x     x  xx         xxx  xx     xxxxxx\n"
+"xxxxxx           xx         x    x    x  xx         xx   xx        xxx\n"
+"xxxx  xx           xx        xx  xx   x  xxx       xxx   xxx       xxx\n"
+"xxxx    xxx          xx        x  x   x   xxx     xxx     xxxx     xxx\n"
+" xx          xx         x       x  x xx     xxxxxxx         xxxxxxxx  \n"
+"  xx                             xxxxx                                \n"
+"                           x       xxx                                \n"
+"                    xx xxx  xxxxx   xx                                \n"
+"                             xxxxxx                                   "
+)
+try:
+	import readline
+except ImportError:
+	print "Module readline not available."
+else:
+	import rlcompleter
+readline.parse_and_bind("tab: complete")
