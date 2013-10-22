7c8fea5ba6eac030045e1cf56ded47876fb37a2a
simon <simon.klemenc@gmail.com>
2013-10-22 16:50:51 +0200

    updated BezierClass

--- Ribs/ballooning.py
+++ Ribs/ballooning.py
@@ -2,11 +2,33 @@ __author__ = 'simon'
 
 import numpy
 from Utils.Bezier import BezierCurve
-#TODO ballooning -> upper: beziercurve, lower beziercurve, mapx, amount (total(integral),maximal)
+#TODO ballooning -> amount (total(integral),maximal)
 class Ballooning(object):
-    def __init__(self, points=[]):
-        self.upper=BezierCurve(points[0])
-        self.lower=BezierCurve(points[1])
+    def __init__(self, points=[[[0, 0], [0.2, 0.04], [0.8, 0.04], [1, 0]],
+                               [[0, 0], [0.2, -0.04], [0.8, -0.04], [1, 0]]]):
+        self.upper = BezierCurve(points[0])
+        self.lower = BezierCurve(points[1])
 
-    def mapx(self,xvals):
-        return 
+    def __getitem__(self, xval):
+        if -1 <= xval < 0:
+            return self.upper.xpoint(-xval)
+        elif 0<= xval <= 1:
+            return self.lower.xpoint(xval)
+        else:
+            ValueError("Ballooning only between -1 and 1")
+
+    def mapx(self, xvals):
+        return [self[i] for i in xvals]
+
+    def amount_maximal(self):
+        pass
+
+    def amount_integral(self):
+        pass
+
+    def amount_set(self,amount):
+        factor=float(amount)/self.Amount
+        self.upper.ControlPoints = [i*[1, factor] for i in self.upper.ControlPoints]
+        self.lower.ControlPoints = [i*[1, factor] for i in self.lower.ControlPoints]
+
+    Amount = property(amount_integral, amount_set)
