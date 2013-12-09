4a676056da758f65fc0c20caba949a0cce72cdc5
hiaselhans <simon.klemenc@gmail.com>
2013-12-09 17:13:12 +0100

    package-structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='OpenGlider',
+    version='0.01dev',
+    packages=['openglider',],
+    license='GPL-v3',
+    long_description=open('README.md').read(),
+)
