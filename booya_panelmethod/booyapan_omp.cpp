8640f99953c4fc5c16938fbd8d910eefea9dde59
hiaselhans <simon.klemenc@gmail.com>
2013-12-30 17:05:08 +0100

    panel

--- booya_panelmethod/booyapan_omp.cpp
+++ booya_panelmethod/booyapan_omp.cpp
@@ -46,6 +46,21 @@ struct Panel {
 		int position; //we do have those wake panels inbetween
 };
 
+struct Config
+{
+	float density;
+	float pressure;
+	float farfield_coeff;
+	int casenum;
+	Vector *vinf;
+	int node_number;
+	int panel_number;
+};
+
+enum CONFIG_ARGUMENTS {ARG_Airspeed, ARG_Density, ARG_Pressure, ARG_Casenum, ARG_Wingspan, ARG_Mac, ARG_Sufrace, ARG_Origin, ARG_Farfield, ARG_Error, ARG_Results, ARG_Nodes, ARG_Panels};
+int ARGUMENT_NUMBERS = 13;
+std::string ARGUMENT_WORDS[] = {"AIRSPEED", "DENSITY", "PRESSURE", "CASE_NUM", "WINGSPAN", "MAC", "SURFACE", "ORIGIN", "FARFIELD", "ERROR", "RESULTS", "NODES", "PANELS"};
+
 int num = 0;
 
 
@@ -60,20 +75,117 @@ void *calculate_rows(void*);
 void usage();
 //////////////////////////////////////////////////////////////////
 
+bool read_config(std::ifstream *inputfile, Config *config, bool &found_nodes) {
+	using namespace std;
+	char thisline[100];
+	//string thisline;
+	char *argument;
+	int casenum;
+	float airspeed;
+	Vector *vinf;
+
+	while(inputfile->getline(thisline, max_linelength)){
+		if (thisline[0] == '#' or strlen(thisline) < 2) {
+			//cout << "comment" << endl;  //comment or senseless
+			continue;
+		}
+		//cout << "1" << endl;
+		int number = -1;
+		//cout << thisline << endl;
+		argument = strtok(thisline, " ");
+		//cout << "1.5" << endl;
+		while (true) {
+			number ++;
+			//cout << number << "!" << endl;
+			if (number >= ARGUMENT_NUMBERS) {
+				cout << "Argument \""<< argument << "\"not known." << endl;
+				break;
+			}
+			//cout << "n1" << endl;
+			//cout << "n2" << endl;
+			if (ARGUMENT_WORDS[number] == string(argument))
+				break;
+		}
+		//cout << "2" <<endl;
+//enum CONFIG_ARGUMENTS {ARG_Airspeed, ARG_Density, ARG_Pressure, ARG_Casenum, ARG_Wingspan, ARG_Mac, ARG_Sufrace, ARG_Origin, ARG_Farfield, ARG_Error, ARG_Results, ARG_Nodes, ARG_Panels};
+		switch(number){
+			case ARG_Airspeed:
+				airspeed = atof(strtok(NULL, " "));
+				cout << "Airspeed found: " << airspeed << endl;
+				break;
+			case ARG_Density:
+				config->density = atof(strtok(NULL, " "));
+				cout << "Density found: " << config->density << endl;
+				break;
+			case ARG_Pressure:
+				config->pressure = atof(strtok(NULL, " "));
+				cout << "Pressure found: " << config->pressure << endl;
+				break;
+			case ARG_Casenum:
+				int casenum;
+				casenum = atoi(strtok(NULL, " "));
+				//cout << "casenum: " << casenum << endl;
+				config->casenum = casenum;
+				config->vinf = (Vector*)malloc(sizeof(Vector) * config->casenum);
+				float *aoa;
+				aoa = (float*)malloc(sizeof(float)*2*casenum);
+				char *value;
+				
+				for(int j=0;j<2; j++){
+					//cout << "no";
+					inputfile->getline(thisline, max_linelength);
+					value = strtok(thisline, " ");
+					//cout << "jo";
+					for(int i=0; i<casenum; i++){
+						
+						*(aoa + i + j*casenum) =atof(value);
+						//cout << value << "/" << i+j*casenum << endl;
+						value = strtok(NULL, " ");
+						//cout << "ui" << endl;
+					}
+				}
+				//cout << "p2" << endl;
+				for(int i=0; i<casenum; i++){
+					Vector *vect = config->vinf + i;
+					//almost Spherical coordinates (aoa1[z]=90-theta, aoa2[y]=phi)
+					(*vect)(0) = airspeed * cos(*(aoa + i)* pi / 180) * cos(*(aoa + i + casenum)* pi / 180);
+					(*vect)(1) = airspeed * cos(*(aoa + i)* pi / 180) * sin(*(aoa + i + casenum)* pi / 180);
+					(*vect)(2) = airspeed * sin(*(aoa + i)* pi / 180);					
+				}
+				cout << "Casenum found: " << config->casenum << endl;
+				break;
+			case ARG_Farfield:
+				config->farfield_coeff = atof(strtok(NULL, " "));
+				cout << "Farfield found: " << config->farfield_coeff << endl;
+				break;
+			case ARG_Nodes:
+				config->node_number = atoi(strtok(NULL, " "));
+				found_nodes = true;
+				return true;
+			case ARG_Panels:
+				config->panel_number = atoi(strtok(NULL, " "));
+				found_nodes = false;
+				return true;
+		}
+		//cout << "3" << endl;
+	}
+	return false;
+}
+
 
 int main(int argc, char* argv[]){
-
 	using namespace std;
 	cout << "---------------------------------------------------------------------" << endl;
 	cout << "--------------------------Booya-Panelmethod--------------------------" << endl;
 	cout << "---------------------------------------------------------------------" << endl;
 
 
-
+	float airspeed, density, pressure, farfield_coeff;
+	int casenum, node_number, panel_number;
 	int num_threads = 1;
 	char* inpath;
     char* outpath;
-
+    bool found_nodes = false;
 	//Parse Command Line
 	if (argc < 3) { // more than 2 arguments, or do nothing.
         usage(); // usage
@@ -93,7 +205,9 @@ int main(int argc, char* argv[]){
             	num_threads = atoi(argv[i+1]);
             }
             }
+        //cout << outpath << endl;
         if (outpath == "") outpath = inpath; //TODO: Append or change ending
+
 
     	ifstream inputfile (inpath);
 
@@ -102,11 +216,40 @@ int main(int argc, char* argv[]){
     	cout << "use " << num_threads << " cores" << endl;
 
 
+    	char thisline[max_linelength];
+    	char *value;
+    	int i;
+    	Vector *nodes;
 
 
 
 		cout << "Loading File: " << inpath << endl << endl;
-		
+		Config config;
+		while (read_config(&inputfile, &config, found_nodes)) {
+			if (found_nodes) {
+				////////////////////NODES SECTION//////////////////////////////
+				nodes = (Vector*)malloc(sizeof(Vector) * config.node_number);
+				while(inputfile.getline(thisline, max_linelength) && i < config.node_number) {
+					if (thisline[0] == '#' or strlen(thisline) < 2)
+						continue;
+
+					value = strtok(thisline, " ");
+					if (string(value) == "PANELS") {
+						//cout << "PANLS";
+						config.panel_number = atoi(strtok(NULL, " "));
+						break;
+					}
+					for (int j=0; j<3; j++){
+						nodes[i][j] = atof(value);
+						value = strtok(NULL," ");
+					}
+					i++;
+				}
+			} else {
+				break;
+			}
+		}
+/*
 		char thisline[max_linelength];
 		char* value;
 		char* argument;
@@ -115,55 +258,81 @@ int main(int argc, char* argv[]){
 			if (thisline[0] == '#' || strlen(thisline) < 2)	{
 				continue; //Comment
 			}
-			cout << thisline << endl;
-			value = strtok(thisline, " ");
-			argument = strtok(NULL, " ");
-			config[value] = argument;
+			int number = -1;
+			//cout << thisline << endl;
+			argument = strtok(thisline, " ");
 
-			if (string(value) == "NODES")	{
-				num = atoi(argument);
-				break;
+			while (true) {
+				number ++;
+				if (number >= ARGUMENT_NUMBERS) {
+					cout << "Argument \""<< argument << "\"not known." << endl;
+					break;
+				}
+				if (ARGUMENT_WORDS[number] == string(argument))
+					break;
 			}
+			switch(number){
+				case Airspeed:
+					cout << "Airspeed found" << endl;
+					break;
+				case Density:
+					cout << "Density found" << endl;
+					break;
+			}
+
+
+
+
+
+
+
+			// argument = strtok(NULL, " ");
+			// config[value] = argument;
+
+			// if (string(value) == "NODES")	{
+			// 	num = atoi(argument);
+			// 	break;
+			// }
 			}
 		//////////////////////Move down
-		cout << "Airspeed:  " << config["AIRSPEED"] << endl;
-		cout << "Density:  " << config["DENSITY"] << endl;
-		cout << "Pressure [Pa]:  " << config["PRESSURE"] << endl;
-		cout << "Number of cases:  " << config["CASENUM"] << endl;
+		//cout << "Airspeed:  " << config["AIRSPEED"] << endl;
+		//cout << "Density:  " << config["DENSITY"] << endl;
+		//cout << "Pressure [Pa]:  " << config["PRESSURE"] << endl;
+		//cout << "Number of cases:  " << config["CASENUM"] << endl;
 		//casenum=3 -> anglesofattack: 0 0 0; sideslip_angles: 0 0 0
-		cout << "WINGSPAN: " << config["WINGSPAN"] << endl;
-		cout << "SURFACE: " << config["SURFACE"] << endl;
+		//cout << "WINGSPAN: " << config["WINGSPAN"] << endl;
+		//cout << "SURFACE: " << config["SURFACE"] << endl;
 		//cout << "NODES: " << config["NODES"] << endl;
 		// ERROR, COLLDIST, FARFIELD=5, 
 		//Results: coefficients, forces, geometry, velocity, pressure, center points, doublet values, source values, velocity components, mesh caracteristics, static pressure, dynamic pressure, manometer pressure (1/0)
-		
+		*/
 		////////////////////////////////////////////NODES
-		cout << "NODES: " << num << endl;
+		//cout << "NODES: " << num << endl;
 
-		Vector nodes[num];
-		int num_panels = 0;
+		
+		int num_panels = config.panel_number;
 
-		int i = 0;
-		while(inputfile.getline(thisline, max_linelength)) {
-			if (thisline[0] == '#' or strlen(thisline) < 2) {
-				//cout << "comment" << endl;
-				continue;
-			}
+		i = 0;
+		// while(inputfile.getline(thisline, max_linelength)) {
+		// 	if (thisline[0] == '#' or strlen(thisline) < 2) {
+		// 		//cout << "comment" << endl;
+		// 		continue;
+		// 	}
 
 
-			value = strtok(thisline, " ");  //END OF NODE-SECTION
-			if (string(value) == "PANELS") {
-				//cout << "PANLS";
-				num_panels = atoi(strtok(NULL, " "));
-				break;
-			}
-			for (int j=0; j<3; j++){
-				nodes[i][j] = atof(value);
-				value = strtok(NULL," ");
-			}
-			i++;
+		// 	value = strtok(thisline, " ");  //END OF NODE-SECTION
+		// 	if (string(value) == "PANELS") {
+		// 		//cout << "PANLS";
+		// 		num_panels = atoi(strtok(NULL, " "));
+		// 		break;
+		// 	}
+		// 	for (int j=0; j<3; j++){
+		// 		nodes[i][j] = atof(value);
+		// 		value = strtok(NULL," ");
+		// 	}
+		// 	i++;
 
-		}
+		// }
 		//cout << nodes[0] << endl;
 		cout << "PANELS: " << num_panels;
 		///////////////////////////////////////////PANELS/WAKE
@@ -182,6 +351,7 @@ int main(int argc, char* argv[]){
 			if (thisline[0] == '#' or strlen(thisline) < 2) {
 				continue;
 			}
+			//cout << "lese" << endl;
 			panel = panels + i;
 			i++;
 			value = strtok(thisline, " ");
@@ -194,32 +364,32 @@ int main(int argc, char* argv[]){
 				panel->position=num_not_wake;
 				num_not_wake++;
 			}
-			panel->p1 = atoi(strtok(NULL, " "));
-			panel->p2 = atoi(strtok(NULL, " "));
-			panel->p3 = atoi(strtok(NULL, " "));
+			panel->p1 = atoi(strtok(NULL, " ")) - 1;
+			panel->p2 = atoi(strtok(NULL, " ")) - 1;
+			panel->p3 = atoi(strtok(NULL, " ")) - 1;
 			if (value[0] == '2')
 				panel->p4 = panel->p3; //3-node
 			else
-				panel->p4 = atoi(strtok(NULL, " ")); //4-node
+				panel->p4 = atoi(strtok(NULL, " ")) - 1; //4-node
 
-			panel->neighbour_front = atoi(strtok(NULL, " "));
-			panel->neighbour_back = atoi(strtok(NULL, " "));
+			panel->neighbour_front = atoi(strtok(NULL, " ")) - 1;
+			panel->neighbour_back = atoi(strtok(NULL, " ")) - 1;
 
 			if(!panel->wake){
-				panel->neighbour_left = atoi(strtok(NULL, " "));
+				panel->neighbour_left = atoi(strtok(NULL, " ")) - 1;
 				if (value[0] == '2')
 					panel->neighbour_right = panel->neighbour_left;
 				else
-					panel->neighbour_right = atoi(strtok(NULL, " "));
+					panel->neighbour_right = atoi(strtok(NULL, " ")) - 1;
 			}
 			///////////////////////////////////////////////////////////////////////////
 			///////////////////////CALCULATE PARAMETERS////////////////////////////////
 			//Could be multithreaded aswell?
 			//subtract one because the pointnrs. start at 1
-			p1 = nodes + panel->p1 -1;
-			p2 = nodes + panel->p2 -1;
-			p3 = nodes + panel->p3 -1;
-			p4 = nodes + panel->p4 -1;
+			p1 = nodes + panel->p1;
+			p2 = nodes + panel->p2;
+			p3 = nodes + panel->p3;
+			p4 = nodes + panel->p4;
 
 			panel->norm_vect = (*p3-*p1).cross(*p4-*p2);
 			panel->area = panel->norm_vect.norm();
@@ -232,7 +402,7 @@ int main(int argc, char* argv[]){
 			panel->tang_vect_span = ((*p3+*p4)/2-panel->r_center);
 			panel->tang_vect_span.normalize();
 			panel->tang_vect_chord = panel->tang_vect_span.cross(panel->norm_vect);
-			panel->sigma = panel->norm_vect.dot(Eigen::Vector3d(12.,0.,2.));
+			panel->sigma = panel->norm_vect.dot(*(config.vinf));
 
 		}
 		inputfile.close();
@@ -242,12 +412,18 @@ int main(int argc, char* argv[]){
 		/////////////////////////////////////////////////////////////////////////
 		////////////////////GENERATE MATRIX/(LHS+RHS)////////////////////////////
 		Eigen::MatrixXf matrix(num_not_wake, num_not_wake);
-		Eigen::VectorXf rhs[2](num_not_wake);
+		Eigen::VectorXf rhs(num_not_wake);
 		cout << "CALCULATE MATRIX" << endl;
 		//Vectors+Matrices already zeroed out
 		boost::progress_display show_progress( num_panels );
 
-		//CALCULATE PARALLELL	
+		rhs.setZero();
+		matrix.setZero();
+
+		//CALCULATE PARALLELL
+
+		//cout << "---------------------------------------------------" << endl;
+		//int counter = 0;	
 
 
 
@@ -273,23 +449,24 @@ int main(int argc, char* argv[]){
 					r_diff = panel_j->r_center - panel_i->r_center;
 					pn = r_diff.dot(panel_i->norm_vect);
 					dist = r_diff.norm();
+					//cout << "dist: " << dist << endl;
 
 					if(dist>faktor*panel_i->smp_len*panel_i->smq_len){
 						farfield_calc(pn, panel_i->area, dist, cjk, bjk);
 						}
 					else {
-						nearfield_calc(panel_i, nodes, &(panel_j->r_center), cjk, bjk);
+						//nearfield_calc(panel_i, nodes, &(panel_j->r_center), cjk, bjk);
 						}
 
 					if (panel_i->wake){ //wake -> two neighbours (CHECK SIGNS!! maybe sign(dot(normvecs))))
 						//cout << panel_i->neighbour_front << "//" << panel_i->neighbour_back << endl;
 						//the neighbours are out of range or are wake panels
-						if (panel_i->neighbour_front < 1 || panel_i->neighbour_front > num_panels || (panels + panel_i->neighbour_front - 1)->wake)
+						if (panel_i->neighbour_front < 0 || panel_i->neighbour_front >= num_panels || (panels + panel_i->neighbour_front)->wake)
 							cout << "Error in panel " << i << ": neighbour front not right!" << endl;
-						if (panel_i->neighbour_back < 1 || panel_i->neighbour_back > num_panels || (panels + panel_i->neighbour_back - 1)->wake)
+						if (panel_i->neighbour_back < 0 || panel_i->neighbour_back >= num_panels || (panels + panel_i->neighbour_back)->wake)
 							cout << "Error in panel " << i << ": neighbour back not right!" << endl;
-						matrix((panels + panel_i->neighbour_front -1)->position, panel_j->position)+=cjk;
-						matrix((panels + panel_i->neighbour_back - 1)->position, panel_j->position)-=cjk;
+						matrix((panels + panel_i->neighbour_front)->position, panel_j->position)+=cjk;
+						matrix((panels + panel_i->neighbour_back)->position, panel_j->position)-=cjk;
 						}
 					else if(!(panel_i->wake)){ //normal panel!
 						if(panel_j->wake || panel_i->wake || panel_i->position < 0 || panel_j->position<0)
@@ -300,31 +477,42 @@ int main(int argc, char* argv[]){
 						// if(panel_j->position > num_not_wake-10)
 						// 	cout << "oops, j=" << panel_j->position << " / " << num_not_wake << endl;
 						matrix(panel_i->position, panel_j->position) = cjk;
-						rhs(panel_j->position) += bjk*panel->sigma;
+						//cout << panel->norm_vect.dot(*(config.vinf+0)) << endl;
+						rhs(panel_j->position) += bjk*panel_j->norm_vect.dot(*(config.vinf+0));
 						}
 					
 
 					}
+			//progressbar
+			//counter++;
+			//if((counter*100)%num_panels == 0) cout << "*" << flush;
 		
 			++show_progress;
 			}
 		}
 
 
+		//cout << rhs << endl;
+		cout << "area: " << panels->area << endl;
+		cout << "mat: " << panels->r_center << endl;
 
 
 
-		cout << "Solving System" << endl;
+
+
+		cout << endl << "Solving System" << endl;
+		cout << rhs(0) << endl;
 
 		Eigen::VectorXf results(num_not_wake);
+		results.setZero();
 		//Eigen::LU<Eigen::MatrixXf> lumatrix(matrix);
 		//lumatrix.solve(rhs, &results);
 		//---->TODO:	more than one aoa
 		//				keep rhs
 		//				generate lu-matrix once only!
-		matrix.lu().solve(rhs);
+		results = matrix.lu().solve(rhs);
 		cout << "SOLVED!!! :)" << endl;
-		cout << rhs << endl;
+		cout << results(2) << endl;
 
 	}
 
@@ -524,11 +712,10 @@ void nearfield_calc(Panel *panel, Vector *points, Vector *r_diff, float &lhs_coe
 				lhs_temp = atan2(rnum, dnom);
 
 			rhs_temp=blabla*gl-norm_dist*lhs_temp;
-			}
-
+		}
 		lhs_coeff += lhs_temp;
 		rhs_coeff += rhs_temp;
-		}
+	}
 
 		// if(vergleich(panelpunkte[i],panelpunkte[i+1])||(norm3d(h)/norm3d(s)<=coresize&&imp3d(a,s)>=0.&&imp3d(b,s)<=0.)||norma<=coresize||normb<=coresize){
 		// 	cjki=0.;
