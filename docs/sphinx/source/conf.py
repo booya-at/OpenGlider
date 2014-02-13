5cb7449ea673660bd1c8c77f5097bc5ebf18166b
hiaselhans <simon@booya.at>
2014-02-13 16:34:34 +0100

    sphinx

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -20,10 +20,9 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.dirname(
     os.path.dirname(
-        os.path.dirname(
-            os.path.dirname(
-                os.path.abspath('.'))))))
+        os.path.abspath('.'))))
 
+#print(sys.path)
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
