b349abbd29d8e9eeabdd6e019eff50ad5f2a82d3
hiaselhans <simon@booya.at>
2014-02-13 02:25:23 +0100

    a

--- testall.py
+++ testall.py
@@ -1,37 +1,36 @@
 #!/bin/python2
 
 from optparse import OptionParser
+import sys
 try:
     import unittest2 as unittest
 except (ImportError, NameError):
     import unittest
 
-def main():
 
-    parser = OptionParser()
-    parser.add_option("-n", "--num", default=1, help="Number of loops")
-    parser.add_option("-a", "--run_all", action='store_true', help="Run all tests (including visual)")
-    parser.add_option("-p", "--pattern", help="Run a custom Pattern to find")
-    parser.add_option("-f", "--folder", default="tests")
-    parser.add_option("-v", "--verbose", default=2)
+parser = OptionParser()
+parser.add_option("-n", "--num", default=1, help="Number of loops")
+parser.add_option("-a", "--run_all", action='store_true', help="Run all tests (including visual)")
+parser.add_option("-p", "--pattern", help="Run a custom Pattern to find")
+parser.add_option("-f", "--folder", default="tests")
+parser.add_option("-v", "--verbose", default=2)
 
-    args = parser.parse_args()[0]
+args = parser.parse_args()[0]
 
-    if args.pattern:
-        pattern = args.pattern
-    elif args.run_all:
-        pattern = "*test*.py"
-    else:
-        pattern = "test*.py"
+if args.pattern:
+    pattern = args.pattern
+elif args.run_all:
+    pattern = "*test*.py"
+else:
+    pattern = "test*.py"
 
-    loader = unittest.TestLoader().discover(args.folder, pattern)
+loader = unittest.TestLoader().discover(args.folder, pattern)
 
-    for i in range(int(args.num)):
-        print("\n\n>>> Running ("+str(i+1)+"/"+str(args.num)+")")
-        test_results = unittest.TextTestRunner(verbosity=int(args.verbose)).run(loader)
-        print(">>> Errors: " + str(test_results.errors))
-        print(">>> Failures: " + str(test_results.failures))
+for i in range(int(args.num)):
+    print("\n\n>>> Running ("+str(i+1)+"/"+str(args.num)+")")
+    test_results = unittest.TextTestRunner(verbosity=int(args.verbose)).run(loader)
+    print(">>> Errors: " + str(test_results.errors))
+    print(">>> Failures: " + str(test_results.failures))
 
-    if i > 0:
-        return test_results.wasSuccessful()
+sys.exit(test_results.wasSuccessful())
 
