2374915d57ebd958394a466a81a1b6799ff433f0
hiaselhans <simon@booya.at>
2014-03-13 00:15:47 +0100

    fix

--- setup.py
+++ setup.py
@@ -25,13 +25,57 @@ import os
 import openglider
 
 
+
+from distutils.core import setup
+import setuptools
+import os
+
+packages, package_data = [], {}
+# This is all copied 1:1 from django-project as i dont know any better way to do this
+def fullsplit(splitpath, result=None):
+    """
+Split a pathname into components (the opposite of os.path.join)
+in a platform-neutral way.
+"""
+    if result is None:
+        result = []
+    head, tail = os.path.split(splitpath)
+    if head == '':
+        return [tail] + result
+    if head == splitpath:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+og_dir = 'openglider'
+
+for dirpath, dirnames, filenames in os.walk(og_dir):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
+    if '__init__.py' in filenames:
+        packages.append(package_name)
+    elif filenames:
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.append(parts.pop())
+        relative_path.reverse()
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames])
+
 root_dir = os.path.dirname(__file__)
 setup(
     name='OpenGlider',
     version=openglider.__version__,
     #version='0.01dev',
-    packages=['openglider'],
-    include_package_data=True,
+    packages=packages,
+    package_data=package_data,
+    #include_package_data=True,
     license='GPL-v3',
     long_description=open('README.md').read(),
     requires=['numpy', 'dxfwrite', 'ezodf2', 'scipy', 'svgwrite', 'matplotlib'],
