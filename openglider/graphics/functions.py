c4a3061d87a084fc626cd20319e9c40f3c30f43e
hiaselhans <simon@booya.at>
2014-06-27 20:07:48 +0200

    travisfix + refractoring (graphics)

---
+++ openglider/graphics/functions.py
@@ -0,0 +1,53 @@
+#! /usr/bin/python2
+# -*- coding: utf-8; -*-
+#
+# (c) 2013 booya (http://booya.at)
+#
+# This file is part of the OpenGlider project.
+#
+# OpenGlider is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# OpenGlider is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenGlider.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import vtk
+import numpy
+
+from openglider.vector import depth, norm, normalize
+# Quick graphics lib to imitate mathematicas graphics functions
+
+
+def tofloat(lst):
+    if isinstance(lst, list):
+        return map(tofloat, lst)
+    else:
+        return float(lst)
+
+
+def __isintlist(arg):
+    if depth(arg) > 1:
+        return max([__isintlist(i) for i in arg])
+    else:
+        if isinstance(arg, int):
+            return 0
+        else:
+            return 1
+
+
+def _isintlist(arg):
+    if __isintlist(arg) == 0:
+        return True
+    else:
+        return False
+
+
+
---
+++ openglider/graphics/qt.py
@@ -0,0 +1,30 @@
+from PyQt4 import QtGui
+import vtk
+from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
+
+class GraphicsWidget(QtGui.QWidget):
+    def __init__(self, *graphics):
+        super(GraphicsWidget, self).__init__()
+        self.gridlayout = QtGui.QGridLayout(self)
+        self.renderer = vtk.vtkRenderer()
+        self.vtkWidget = QVTKRenderWindowInteractor(self)
+        #print(dir(self))
+        self.gridlayout.addWidget(self.vtkWidget)
+
+        self.render_window = self.vtkWidget.GetRenderWindow()
+        self.render_window.AddRenderer(self.renderer)
+        self.render_interactor = self.render_window.GetInteractor()
+        self.render_interactor.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
+        self.renderer.SetBackground(0.1, 0.2, 0.4)
+        self.renderer.ResetCamera()
+
+        self.add_graphics(*graphics)
+
+    def add_graphics(self, *graphics):
+        for graph in graphics:
+            self.renderer.AddActor(graph.actor)
+
+    def show(self):
+        self.render_interactor.Initialize()
+        self.render_interactor.Start()
