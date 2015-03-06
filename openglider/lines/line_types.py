fix line stretch calculation, add line stretch interpolation

openglider/lines/line_types.py
@@ -17,9 +17,43 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with OpenGlider.  If not, see <http://www.gnu.org/licenses/
-import collections
+from __future__ import division
 
-LineType = collections.namedtuple("LineType", ['name', 'cw', 'thickness', 'stretch'])
+import scipy.interpolate
 
-liros = LineType('liros', 1.1, 0.002, 0.01)
-liros160 = LineType('liros160', 1.1, 0.002, 0.01)
+
+class LineType():
+    def __init__(self, name, cw, thickness, stretch_curve):
+        self.name = name
+        self.cw = cw
+        self.thickness = thickness
+        if stretch_curve[0][0] != 0:
+            stretch_curve.insert(0, [0, 0])
+        self.stretch_interpolation = scipy.interpolate.interp1d([p[0] for p in stretch_curve],
+                                                                [p[1] for p in stretch_curve],
+                                                                bounds_error=False)
+
+    def get_stretch_factor(self, force):
+        return 1 + self.stretch_interpolation(force) / 100
+
+
+
+# SI UNITS -> thickness [m], stretch [N, %]
+
+liros = LineType('liros', 1.1, 0.002, [[100, 1]])
+liros160 = LineType('liros160', 1.1, 0.002, [[100, 1]])
+
+
+cousin12100 = LineType("Cousin 12100", 1.1, 0.0006, [[50, 0.4],
+                                                     [100, 0.9],
+                                                     [150, 1.3],
+                                                     [200, 1.5],
+                                                     [250, 1.9]])
+
+
+cousin16140 = LineType("Cousin 16140", 1.1, 0.0007, [[50, 0.75],
+                                                     [100, 1.],
+                                                     [150, 1.1],
+                                                     [200, 1.4],
+                                                     [250, 1.75],
+                                                     [500, 2.75]])