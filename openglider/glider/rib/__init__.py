77ecd2ded8dc60b5b1771b9d51e4cc211fde980a
looooo <sppedflyer@gmail.com>
2017-08-23 08:19:49 +0200

    add class SingleSkinRib

--- openglider/glider/rib/__init__.py
+++ openglider/glider/rib/__init__.py
@@ -1,3 +1,3 @@
 from openglider.glider.rib.elements import AttachmentPoint, GibusArcs, RibHole, RigidFoil
-from openglider.glider.rib.rib import Rib
+from openglider.glider.rib.rib import Rib, SingleSkinRib
 from openglider.glider.rib.minirib import MiniRib
