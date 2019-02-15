75511ca9a7f34b6bc30776717cf9eb9e46b47df5
Simon Klemenc <simon.klemenc@gmail.com>
2019-02-15 10:37:10 +0100

    config repr

--- openglider/utils/__init__.py
+++ openglider/utils/__init__.py
@@ -102,7 +102,12 @@ class Config(object):
         }
 
     def __repr__(self):
-        return self.__dict__.__repr__()
+        repr_str = "{}\n".format(self.__class__)
+        width = max([len(x) for x in self.__dict__])
+        for key, value in self.__dict__.items():
+            repr_str += "    -{0: <{width}} -> {value}\n".format(key, value=value, width=width)
+
+        return repr_str
 
     def __iter__(self):
         for key, value in self.__dict__.items():
